b6cb0bf16f3ecdb7736b02a22cba481f88d47f2d
Eric Schmiel <eric.schmiel@timmons.com>
2023-10-18 16:02:00 -0400

    added log when receiving req

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // basic express server deployment on ec2 tutorial:
 // https://betterprogramming.pub/deploying-a-basic-express-api-on-amazon-ec2-eea0b54a825
+// PM docs: https://www.npmjs.com/package/pm2
+// Server IP: http://54.86.94.159/
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    console.log('req received: ', req)
     res.send('Hello World!s')
 })
 

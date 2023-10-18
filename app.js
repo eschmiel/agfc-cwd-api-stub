d6ace018e74b4a6bcbb6fb0364775efea3d8e58e
Eric Schmiel <eric.schmiel@timmons.com>
2023-10-18 15:44:15 -0400

    added doc

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// basic express server deployment on ec2 tutorial:
+// https://betterprogramming.pub/deploying-a-basic-express-api-on-amazon-ec2-eea0b54a825
 const express = require('express')
 const app = express()
 const port = 3000

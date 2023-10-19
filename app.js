43deb360cfb9403e948ec85ce514a15a7370abc6
Eric Schmiel <eric.schmiel@timmons.com>
2023-10-19 11:29:40 -0400

    commented out http server

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ https.createServer(options, app).listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
